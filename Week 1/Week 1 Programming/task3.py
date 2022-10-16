d71375f4375d52fbdf008613cb68e34e174141aa
ZooWeeMama <104341286+SweingZ@users.noreply.github.com>
2022-10-16 17:09:48 +0545

    16th oct 2022

---
+++ Week 1/Week 1 Programming/task3.py
@@ -0,0 +1,4 @@
+C=38.4
+F=(C*9/5)+32
+print("Temp in Celcius",C,"C")
+print("Temp in Fahrenheit",F,"F")
---
+++ Week 1/Week 1 Programming/task5.py
@@ -0,0 +1,8 @@
+students=int(input("Enter the number of students: "))
+if students%24==0:
+    groups=students/24
+else:
+    groups=int(students/24) + 1
+print("The number of groups needed is",groups)
+print("The number of full groups",students//24)
+print("Number of students in leftover group",students%24)
