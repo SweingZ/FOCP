4d0a5c4d8f5284ce3d3f6da3e5e20c6b8e5fda54
ZooWeeMama <104341286+SweingZ@users.noreply.github.com>
2022-10-16 12:45:30 +0545

    Delete Week 1 directory

--- Week 1/task3.py
+++
@@ -1,4 +0,0 @@
-C=38.4
-F=(C*9/5)+32
-print("Temp in Celcius",C,"C")
-print("Temp in Fahrenheit",F,"F")
--- Week 1/task5.py
+++
@@ -1,8 +0,0 @@
-students=int(input("Enter the number of students: "))
-if students%24==0:
-    groups=students/24
-else:
-    groups=int(students/24) + 1
-print("The number of groups needed is",groups)
-print("The number of full groups",students//24)
-print("Number of students in leftover group",students%24)
